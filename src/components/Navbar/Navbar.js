4a57041b69deba7aa963b59c819e01e86b079e71
KIRAN M R <iamthekiranmr@gmail.com>
2022-11-25 09:44:40 +0530

    initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="bg-stone-900 flex flex-row justify-between h-14 text-white fixed top-0 left-0 right-0">
+      <h1 className="pl-5 pt-3.5 text-2xl">Search Your Books</h1>
+      
+    </div>
+  );
+}
+
+export default Navbar
